2bade455ca988d86c96bd9df6eae936d4325e939
Jan Černý <jc@ucw.cz>
2026-07-03 17:26:11 +0200

    reorganize jektex name space

--- lib/jektex.rb
+++ lib/jektex.rb
@@ -1,5 +1,10 @@
-module Jektex
-  autoload :VERSION, "jektex/version.rb"
-end
+require "jektex/version"
+require "jektex/configuration"
+require "jektex/reporter"
+require "jektex/renderer"
+require "jektex/cache"
+require "jektex/page_processor"
 
-require "jektex/jektex.rb"
+# when loaded as a Jekyll plugin, Jekyll is always defined; the guard
+# lets the gem be required standalone (for tests and other tooling)
+require "jektex/hooks" if defined?(Jekyll)
